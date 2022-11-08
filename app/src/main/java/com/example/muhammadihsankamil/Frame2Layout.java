1d3ac860369e6afd0df804ef793269bbd08929b0
Heat41 <m.ihsankamil38@gmail.com>
2022-11-08 13:35:05 +0700

    Perubahan2

--- app/src/main/java/com/example/muhammadihsankamil/Frame2Layout.java
+++ app/src/main/java/com/example/muhammadihsankamil/Frame2Layout.java
@@ -25,24 +25,17 @@ public class Frame2Layout extends AppCompatActivity {
             @Override
             public void onClick(View view)  { login(); }
         });
-
-
     }
     public void login(){
         //fungsi login dibuat untuk menampilkan pesan dan berpindah dari satu activity ke
         //activity lainnya
-
         //simpan input dari pengguna ke variabel user
         user = eUserName.getText().toString();
-
         //intent digunakan untuk berpindah activity, dalam kasus ini berpindah dari Frame2Activity
         //yaitu activity saat ini ke LinearActivity
         Intent intent = new Intent(Frame2Layout.this,LinearActivity2.class);
-
         intent.putExtra("user",user);
-
         Toast.makeText(this, "Selamat Datang "+user,Toast.LENGTH_LONG).show();
-
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/muhammadihsankamil/LinearActivity2.java
+++ app/src/main/java/com/example/muhammadihsankamil/LinearActivity2.java
@@ -16,11 +16,9 @@ public class LinearActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear2);
-
         //ambil data dari Intent Frame2Activity
         //pastikan key yang di gunakan sama, apabila tidak sama maka tidak dapat mengambil data
         user = getIntent().getExtras().getString("user");
-
         //hubungkan dengan EditText dengan id Kepada
         eKepada = findViewById(R.id.kepada);
         eSubyek = findViewById(R.id.subyek);
